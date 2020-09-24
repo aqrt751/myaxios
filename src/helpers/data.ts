5e5b6818bf0318d42ab151e76006567828f9517e
unknown <ding.sun@kingstar.com>
2020-09-24 15:08:03 +0800

    testing

--- src/helpers/data.ts
+++ src/helpers/data.ts
@@ -2,6 +2,7 @@ import { isPlainObject } from './util'
 
 export function transformRequest (data: any): any {
     if(isPlainObject(data)) {
+        console.log(11111)
         return JSON.stringify(data)
     }
     return data
